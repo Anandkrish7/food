7bd85ba890eea0e75c7aec4cddf3176b267c41c5
Anandkrish7 <anandkick921@gmail.com>
2023-09-11 18:27:55 +0530

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("login-popup").addEventListener("click",function(){
+    document.querySelector(".wrapper").style.display = "block";
+})
+document.querySelector(".wrapper #xmark").addEventListener("click",function(){
+    document.querySelector(".wrapper").style.display = "none";
+})
+
